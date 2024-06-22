9213fe4d0a50f35eeaa570e45aa9c57294c41206
RaviRaushanSinghj6 <19w91a05j6cse@mriet.ac.in>
2024-06-22 20:07:28 +0530

    Java Code

---
+++ OOPS/StandardLibrary.java
@@ -0,0 +1,11 @@
+package OOPS;
+
+public class StandardLibrary {
+    public static void main(String[] args) {
+        System.out.println(Math.sqrt(24));
+        System.out.println(Math.sqrt(25));
+        System.out.println(Math.floor(5.4));
+        System.out.println(Math.ceil(7.5));
+        return;
+    }
+}
