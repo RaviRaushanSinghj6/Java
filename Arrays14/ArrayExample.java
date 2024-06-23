32ea08065012855f7536b22d1781f94a5aaa7c86
RaviRaushanSinghj6 <19w91a05j6cse@mriet.ac.in>
2024-06-23 23:21:17 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	OOPS/Constructor.java

---
+++ Arrays14/ArrayExample.java
@@ -0,0 +1,19 @@
+package Arrays14;
+
+public class ArrayExample {
+    void Arr()
+    {
+        int[] ages = new int[5];// Type arrayname[] = new type[Size];
+        float [] Weight = new float[5];
+        String [] Name = new String[5];
+
+        ages[0] = 45;
+        Weight[0] = 2.456F;
+        Name[0] = "Ravi";
+
+        System.out.println(ages[0]);
+        System.out.println(Weight[0]);
+        System.out.println(Name[0]);
+
+    }
+}
