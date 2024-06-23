1c83c5b7fdaf73c1c90854dd796f3fcaabeb243e
Ravi Raushan Singh <118049910+RaviRaushanSinghj6@users.noreply.github.com>
2024-06-23 11:58:57 +0530

    Add files via upload

---
+++ OOPS/Constructor.java
@@ -0,0 +1,34 @@
+package OOPS;
+
+public class Constructor {
+    int a;
+    int b;
+
+    Constructor(int x,int y)
+    {
+        System.out.println("Constructor of constructor class called");
+        a = x;
+        b = y;
+    }
+    int add()
+    {
+        int ans = a+b;
+        return ans;
+    }
+    int sub()
+    {
+        int ans = a-b;
+        return ans;
+    }
+}
+class Calling_Constructor{
+    public static void main(String[] args) {
+        Constructor obj = new Constructor(5, 3);
+        System.out.println("Sum of input number is : ");
+        int ans = obj.add();
+        System.out.println(ans);
+
+        Constructor obj2 = new Constructor(2,5);
+        System.out.println(obj2.sub());
+    }
+}
